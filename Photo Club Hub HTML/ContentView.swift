9b1921410b04be511009b1f687cdbc0bad73588b
Peter van den Hamer <13396568+vdhamer@users.noreply.github.com>
2024-09-07 15:14:40 +0200

    Some changes to keep SwiftLint happy with Apple's template code

--- Photo Club Hub HTML/ContentView.swift
+++ Photo Club Hub HTML/ContentView.swift
@@ -48,7 +48,8 @@ struct ContentView: View {
                 try viewContext.save()
             } catch {
                 // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use
+                // this function in a shipping application, although it may be useful during development.
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
@@ -63,7 +64,8 @@ struct ContentView: View {
                 try viewContext.save()
             } catch {
                 // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use
+                // this function in a shipping application, although it may be useful during development.
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
