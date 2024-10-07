5213cfb91dcaa85474ba1c7f03356effccdd9b99
Peter van den Hamer <13396568+vdhamer@users.noreply.github.com>
2024-10-07 22:48:33 +0200

    Added MembershipView and removed forgotten comment lines.

--- Photo Club Hub HTML/Photo_Club_Hub_HTMLApp.swift
+++ Photo Club Hub HTML/Photo_Club_Hub_HTMLApp.swift
@@ -16,64 +16,6 @@ struct PhotoClubHubHtmlApp: App {
         OrganizationType.initConstants() // creates records for club, museum, and unknown
     }
 
-        /*
-        {
-            "name": {
-                "givenName": "Hans",
-                "infixName": "",
-                "familyName": "Krüsemann"
-            },
-            "optional": {
-                "level3URL": "http://www.vdhamer.com/fgDeGender/Hans_Krusemann/",
-                "roles": {
-                    "isOther": true
-                },
-                "membershipStartDate": "2016-04-01",
-                "nlSpecific": {
-                    "fotobondNumber": 1620090
-                }
-            }
-        }
-
-        {
-            "name": {
-                "givenName": "Jelle",
-                "infixName": "van de",
-                "familyName": "Voort"
-            },
-            "optional": {
-                "level3URL": "http://www.vdhamer.com/fgDeGender/Jelle_van_de_Voort/",
-                "roles": {
-                    "isChairman": true
-                },
-                "membershipStartDate": "2020-01-01",
-                "nlSpecific": {
-                    "fotobondNumber": 1620103
-                }
-            }
-        }
-    }
-
-    {
-        "name": {
-            "givenName": "Peter",
-            "infixName": "van den",
-            "familyName": "Hamer"
-        },
-        "optional": {
-            "birthday": "1957-10-18",
-            "website": "https://glass.photo/vdhamer",
-            "level3URL": "http://www.vdhamer.com/fgDeGender/Peter_van_den_Hamer/",
-            "roles": {
-                "isAdmin": true
-            },
-            "membershipStartDate": "2024-01-01",
-            "nlSpecific": {
-                "fotobondNumber": 1620110
-            }
-        }
-    } */
-
     var body: some Scene {
         Window("Photo Club Hub HTML", id: "mainWindow") {
             ContentView()
---
+++ Photo Club Hub HTML/Views/MembershipView.swift
@@ -0,0 +1,18 @@
+//
+//  MembershipView.swift
+//  Photo Club Hub HTML
+//
+//  Created by Peter van den Hamer on 07/10/2024.
+//
+
+import SwiftUI
+
+struct MembershipView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MembershipView()
+}
