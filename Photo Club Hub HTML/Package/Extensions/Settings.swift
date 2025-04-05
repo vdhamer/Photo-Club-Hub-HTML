8ae4821f6229f737bd7e37f793685370c9831635
Peter van den Hamer <13396568+vdhamer@users.noreply.github.com>
2025-04-05 23:16:27 +0200

    Synchronized Settings folder

--- Photo Club Hub HTML/Package/Extensions/Settings.swift
+++ Photo Club Hub HTML/Package/Extensions/Settings.swift
@@ -9,6 +9,21 @@ import Foundation
 
 struct Settings {
 
+    // Doesn't really work in Photo Club Hub HTML until the version numbers are synchronized
+    static var dataResetPending273: Bool { // stored as a string shown in Settings
+        // returns true until a reset is done
+
+        if UserDefaults.standard.object(forKey: "dataResetPending273") == nil {
+            UserDefaults.standard.set(true, forKey: "dataResetPending273") // interpret nil as true
+            UserDefaults.standard.removeObject(forKey: "dataResetPending272") // cleanup of any old reset key-value pair
+            UserDefaults.standard.removeObject(forKey: "dataResetPending") // cleanup of any old reset key-value pair
+        }
+
+        let prevValue = UserDefaults.standard.bool(forKey: "dataResetPending273")
+        UserDefaults.standard.set(false, forKey: "dataResetPending273") // never true more than once
+        return prevValue // if true, app has to react immediately (by executing data reset)
+    } // implicit getter only
+
     static var manualDataLoading: Bool { // controlled by toggle in Settings
         // Setting this to true clears the existing database and skips loading any data on app startup.
         // It displays "Manual loading" in the Prelude startup screen as a warning that the mode is set.
