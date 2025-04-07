b686ca85e08d5c74161dca8c422379e257f5ce86
Peter van den Hamer <13396568+vdhamer@users.noreply.github.com>
2025-04-08 01:14:00 +0200

    Missing commas in JSON file

---
+++ Photo Club Hub HTML/Package/Photo Club Hub HTMLTests/Photo_Club_Hub_HTMLTests.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_Club_Hub_HTMLTests.swift
+//  Photo Club Hub HTMLTests
+//
+//  Created by Peter van den Hamer on 08/04/2025.
+//
+
+import Testing
+@testable import Photo_Club_Hub_HTML
+
+struct PhotoClubHubHTMLTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Photo Club Hub HTMLTests/Photo_Club_Hub_HTMLTests.swift
@@ -0,0 +1,16 @@
+//
+//  Photo_Club_Hub_HTMLTests.swift
+//  Photo Club Hub HTMLTests
+//
+//  Created by Peter van den Hamer on 08/04/2025.
+//
+
+import Testing
+
+struct Photo_Club_Hub_HTMLTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
