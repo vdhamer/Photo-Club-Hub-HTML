8c74a215079f674e3d908927e484bcb094b26fbb
Peter van den Hamer <13396568+vdhamer@users.noreply.github.com>
2025-09-18 00:01:50 +0200

    Minor change to testing setup.

--- Photo Club Hub HTML/IgniteSource/Level2Site.swift
+++ Photo Club Hub HTML/IgniteSource/Level2Site.swift
@@ -15,7 +15,7 @@ struct Level2Site: Site {
 
     var name: String = "Leden" // set during init()
     // IMPORTANT: http://www.vdhamer.com gives localhost result, http://www.vdhamer.com/fgDeGender works on remote site
-    var url: URL = URL("http://www.vdhamer.com/fkGestel")
+    var url: URL = URL("http://www.vdhamer.com")
     var builtInIconsEnabled: BootstrapOptions = .none
     var author = "Peter van den Hamer"
     let homePage: Members
@@ -99,7 +99,7 @@ struct Level2Site: Site {
 
         self.moc = moc
 
-        let chosenClubIX: Int = 9  // roundabout way to avoid SwiftLint warnings about unused properties
+        let chosenClubIX: Int = 0  // roundabout way to avoid SwiftLint warnings about unused properties
         let clubs = [club0, club1, club2, club3, club4, club5, club6, club7, club8, club9]
         let club = clubs[max(min(chosenClubIX, clubs.count - 1), 0)] // clip to array bounds in case index is wrong
 
