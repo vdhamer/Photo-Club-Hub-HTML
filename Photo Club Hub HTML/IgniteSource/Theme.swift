fbefb4e52d5edf3cca0fca1aaaa5817526dc45bd
Peter van den Hamer <13396568+vdhamer@users.noreply.github.com>
2025-09-23 00:22:15 +0200

    Localization of footnote on Level2 showing when data was generated.

--- Photo Club Hub HTML/IgniteSource/Theme.swift
+++ Photo Club Hub HTML/IgniteSource/Theme.swift
@@ -32,8 +32,9 @@ struct TimeUpdatedFooter: Component { // swiftlint doesn't want this one to be f
 
         Alert {
             Text { // this is Ignite's Text, not SwiftUI's Text
-                String(localized: "Data was last updated on \(date) \(timezoneCode).",
+                String(localized: "This page was last updated on \(date) \(timezoneCode).",
                        table: "HTML",
+                       bundle: .main, // actually the default
                        comment: "Timestamp at bottom of static HTML page showing when page was generate")
             } .horizontalAlignment(.center)
         }
