2b8813f51739dae12c1baa67443b5d5b90c56711
vdhamer <13396568+vdhamer@users.noreply.github.com>
2026-05-26 22:35:42 +0200

    Generate pages into array during init(). Prevents having to do Core Data queries on wrong thread.

--- Photo Club Hub HTML/Ignite/Level 0/Level0Site.swift
+++ Photo Club Hub HTML/Ignite/Level 0/Level0Site.swift
@@ -19,11 +19,36 @@ struct Level0Site: Site {
     let homePage: ExpertisesPage
     let theme = MyTheme()
 
-    init(moc: NSManagedObjectContext, preferences: PreferencesStructHTML) {
-        url = preferences.selectedHost.url(forPath: "expertises") ??
-              URL(preferences.selectedHost.staticString)
+    private let precomputedPages: [any StaticPage] // precomputed to avoid Core Data queries on wrong threads
+    var pages: [any StaticPage] { return precomputedPages }
 
-        self.homePage = Expertises(moc: moc)
+    init(moc: NSManagedObjectContext, preferences: PreferencesStructHTML) {
+        url = preferences.selectedHost.url(forPath: "expertises") ?? URL(preferences.selectedHost.staticString)
+
+        self.homePage = ExpertisesPage(moc: moc) // in future this should change from Site to Page
+
+        let expertiseFetch: NSFetchRequest<Expertise> = Expertise.fetchRequest()
+        expertiseFetch.sortDescriptors = [NSSortDescriptor(key: "id_", ascending: true)]
+        let expertises = (try? moc.fetch(expertiseFetch)) ?? [] // get all expertises
+        if expertises.isEmpty { ifDebugFatalError("No expertises found in Level0Site.init()") }
+
+        let languageFetch: NSFetchRequest<Photo_Club_Hub_Data.Language> = Photo_Club_Hub_Data.Language.fetchRequest()
+        languageFetch.sortDescriptors = [NSSortDescriptor(key: "isoCode_", ascending: true)]
+        let languages = (try? moc.fetch(languageFetch)) ?? []
+        if languages.isEmpty { ifDebugFatalError("No languages found in Level0Site.init()") }
+
+        var pages: [any StaticPage] = [] // start empty
+        for language in languages {
+            for expertise in expertises where LocalizedExpertise.exists(context: moc,
+                                                                        expertiseID: expertise.id,
+                                                                        languageIsoCode: language.isoCode) {
+                // where-clause prevents generating pages without any localizedExpertises - even if Language exits.
+                pages.append(ExpertisePage(expertiseID: expertise.id,
+                                           language: language.isoCode.lowercased(),
+                                           moc: moc))
+            }
+        }
+        self.precomputedPages = pages
     }
 
 }
